b6275f5fc20933223976ec10a0bbc57eab7eecee
Biniyam Getachew <binigetachew18@gmail.com>
2026-02-22 13:13:35 +0300

    restructure API routing to include authentication and user endpoints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import auth 
+from app.api.v1.api import api_router
 
 app = FastAPI(title="Global Connect Ethiopia")
 
 # all URLs in auth.py will now start with /api/v1/auth
-app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
+app.include_router(api_router, prefix="/api/v1", tags=["API Version 1"])
 
 @app.get("/")
 def read_root():
