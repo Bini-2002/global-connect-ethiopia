3bdf3252f5a9da620ce0cbfcebf8b425af7ef6ee
Biniyam Getachew <binigetachew18@gmail.com>
2026-02-22 13:21:32 +0300

    update main.py to include separate routers for authentication and users

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app.api.v1.api import api_router
+from app.api.v1.api import auth , users
 
 app = FastAPI(title="Global Connect Ethiopia")
 
 # all URLs in auth.py will now start with /api/v1/auth
-app.include_router(api_router, prefix="/api/v1", tags=["API Version 1"])
+app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
+app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
 
 @app.get("/")
 def read_root():
