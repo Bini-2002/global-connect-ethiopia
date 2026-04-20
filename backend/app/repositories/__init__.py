ee0b3d2690dc77f5351fd72c5a3c65633e2ab93c
Samrawit-alemu <samrawittalemuu@gmail.com>
2026-04-20 13:41:00 -0700

    chore(repositories): export opportunity repositories

---
+++ backend/app/repositories/__init__.py
@@ -0,0 +1,7 @@
+from app.repositories.opportunity_proposal_repository import OpportunityProposalRepository
+from app.repositories.opportunity_repository import OpportunityRepository
+
+__all__ = [
+    "OpportunityRepository",
+    "OpportunityProposalRepository",
+]
